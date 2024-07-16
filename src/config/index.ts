8852ca028242b79c24d2327809dfa1640099fa76
Mani Meskartehrani <mani.tehrani.work@gmail.com>
2024-07-15 19:21:24 -0700

    Feat: Implementing Landig page + Server

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imgeSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imgeSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imgeSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favorite Icon Picks",
+        href: "#",
+        imgeSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imgeSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Bestsellers Icons",
+        href: "#",
+        imgeSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
